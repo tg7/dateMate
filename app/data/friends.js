6a9c33e14c5695f2d9e3bfc9bf778bdf77bd008f
Todd Germaine <trgermaine@gmail.com>
2016-07-31 22:49:40 -0400

    updated most of the changes

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+{
+  name: 'Alicia',
+  photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiJx7acqp7OAhVGKB4KHf5KAQYQjRwIBw&url=http%3A%2F%2Fdelawarelaw.widener.edu%2Fcurrent-students%2Ffaculty-directory%2Ffaculty%2F118&psig=AFQjCNGQN3ZfnZ5S619sVWGJgdeaoW0eMg&ust=1470075984013831',
+  orientation: female,
+  score: []
+},
+{
+  name: 'Chelsea'
+  photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiEjPiWq57OAhVMkx4KHbS-DLYQjRwIBw&url=http%3A%2F%2Ffavim.com%2Fimage%2F3071676%2F&bvm=bv.128617741,d.dmo&psig=AFQjCNGGMIEfOTUR3WYqwg1JXOtfgtj0hg&ust=1470076188130601',
+  orientation: female,
+  score: []
+},
+{
+  name: 'Alana'
+  photo: 'http://i.telegraph.co.uk/multimedia/archive/03443/carmen_3443958b.jpg',
+  orientation: female,
+  score: []
+},
+{
+  name: 'Faith'
+  photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjQgaTYq57OAhUEJB4KHT9CDxkQjRwIBw&url=http%3A%2F%2Fwww.blackgirldangerous.org%2F2016%2F02%2Fa-lesson-in-loving-myself-bgds-crush-of-the-month-featuring-angelica-ross%2F&bvm=bv.128617741,d.dmo&psig=AFQjCNHPIaEZQPNbL8wJkbuj1QPCPD3kwg&ust=1470076448017736',
+  orientation: female,
+  score: []
+},
+{
+  name: 'Jackson'
+  photo: 'https://pbs.twimg.com/profile_images/1702876689/Sean-Jackson-lg700.jpg',
+  orientation: male,
+  score: []
+},
+{
+  name: 'Paul'
+  photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwj8kLjZrJ7OAhXTth4KHa04AskQjRwIBw&url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fpaul-kovalski-0256772a&psig=AFQjCNGkTfNGIei6UK2Yv5IPzMhNZL0UUA&ust=147007670908407',
+  orientation: male,
+  score: []
+},
+{
+  name: 'Michael'
+  photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjsocarrJ7OAhXKkh4KHa-OBToQjRwIBw&url=http%3A%2F%2Fwww.nj.axa-advisors.com%2FMichael-Sprung.e403575.htm&psig=AFQjCNFJl_kvdr6RPfAlMHZdIR158Nck3A&ust=1470076626691245',
+  orientation: male,
+  score: []
+},
+{
+  name: 'James'
+  photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiupK3JrJ7OAhVKGx4KHbnRBAIQjRwIBw&url=http%3A%2F%2Fwww.ascendancywealth.com%2Fagents.cfm&psig=AFQjCNEod9ZI26UtfiKCKpLqMQMpsIlBxg&ust=1470076653544738',
+  orientation: male,
+  score: []
+}];
+
+
+module.exports = friends;
